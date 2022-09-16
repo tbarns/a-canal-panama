d9746d82c3230efc849ad01ac9420626115fe24b
Timothy Barnaby <tbarnaby1@gmail.com>
2022-09-16 17:24:12 -0500

    added id to index to better connect to the current hour in moment.js; updated  classes to connect to preset CSS.  updated JS to contain a for loop that iterated over text areas to allow current time to be comapred to ids

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,45 @@
 var today = moment();
+// displays time and date 
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-var dateDisplay = document.getElementById("currentDay")
-var container = document.getElementsByClassName("container")
-var row = document.createElement("div")
+var container = $(".container")
+var timeBlocks = $("textarea")
+var currentTime = moment().format("HH");
+console.log(currentTime)
+
+
+for (let index = 0; index < timeBlocks.length; index++) {
+    var id = timeBlocks[index].id;
+    console.log(id)
 
 
 
-// displays time and date 
-dateDisplay.textContent = today
+    if (currentTime < id) {
+        $(timeBlocks).addClass("future");
+    }
+    //
+
+    if (currentTime == id) {
+        $(timeBlocks).addClass("present");
+    }
+    //
+
+     if (currentTime > id) {
+        $(timeBlocks).addClass("past");
+    }
+
+}
+
+//dynamically create timeblocks for standard business hours
+
+//iterate thru all the text areas and compare thier id value to moments currnt time
 
 
 
+
+//ensure that timeblocks change depesding on the time of day
+
+//when we click timeclock it allows input
+
+//
 
