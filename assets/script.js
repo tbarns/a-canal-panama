7dd4251247ea71229748ede7572be02862c12ef9
Timothy Barnaby <tbarnaby1@gmail.com>
2022-09-17 17:26:16 -0500

    added event listern to JS for the save button, started implimenting local stoage

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@ $("#currentDay").text(today.format("MMM Do, YYYY"));
 
 var container = $(".container")
 var timeBlocks = $("textarea")
+var save = $("button")
 var currentTime = moment().format("HH");
 console.log(currentTime)
 
@@ -11,25 +12,27 @@ console.log(currentTime)
 for (let index = 0; index < timeBlocks.length; index++) {
     var id = timeBlocks[index].id;
     console.log(id)
-
-
-
-    if (currentTime < id) {
-        $(timeBlocks).addClass("future");
-    }
-    //
-
-    else if (currentTime == id) {
-        $(timeBlocks).addClass("present");
-    }
-    //
-
-     else if (currentTime > id) {
-        $(timeBlocks).addClass("past");
-    }
-
 }
 
 
-// save button send text area to local storage 
+if (currentTime < id) {
+    $(timeBlocks).addClass("future");
+}
+//
 
+else if (currentTime == id) {
+    $(timeBlocks).addClass("present");
+}
+//
+
+else if (currentTime > id) {
+    $(timeBlocks).addClass("past");
+}
+
+
+
+save.addEventListener("click", saveBtn())
+// save button send text area to local storage
+function saveBtn (){
+localStorage.setItem()
+}
